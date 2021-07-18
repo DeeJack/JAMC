19d55462d8b9af84ed3191aa080d7b6a66fcf981
Lorenzo Fumi <lorenzofumi@gmail.com>
2021-07-18 15:40:20 +0200

    libgdx project setut

---
+++ desktop/src/me/deejack/jamc/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package me.deejack.jamc.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import me.deejack.jamc.JAMC;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new JAMC(), config);
+	}
+}
